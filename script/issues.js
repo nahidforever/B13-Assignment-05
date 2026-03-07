5ffe0a175b195990e856f8aef4095f437c538683
Nahid1828 <isam15-5334@diu.edu.bd>
2026-03-07 13:06:56 +0600

    Implemnt---- Clicking on an issue card will open a modal and show all the information about that Issue.

--- script/issues.js
+++ script/issues.js
@@ -6,6 +6,7 @@ const loadingSpinner = document.getElementById("loadingSpinner");
 const allTab = document.getElementById("allTab");
 const openTab = document.getElementById("openTab");
 const closedTab = document.getElementById("closedTab");
+const issueDetailsModal = document.getElementById("issue-details-modal");
 
 const loadAllIssues = async () => {
   showLoading();
@@ -36,13 +37,14 @@ const displayAllIssues = (issues) => {
     const card = document.createElement("div");
     card.className = `bg-base-100 p-6 rounded-lg shadow-lg space-y-3`;
 
+    card.onclick = () => openIssueModal(issue.id);
     card.innerHTML = `
      <div class="flex justify-between items-center">
               <p></p>
               <p
                 class="bg-[#FEECEC] text-red-600 py-0.5 px-5 rounded-full text-[12px]"
               >
-                ${issue.priority}
+                ${issue.priority.toUpperCase()}
               </p>
             </div>
 
@@ -66,6 +68,7 @@ const displayAllIssues = (issues) => {
               <p>${formattedDate}</p>
             </div>
     `;
+
     allIssueContainer.appendChild(card);
   });
 };
@@ -100,7 +103,7 @@ const activeTab = (btn) => {
 const createElements = (arr) => {
   const htmlElements = arr.map(
     (el) =>
-      `<span class="bg-[#FDE68A] rounded-full px-3 text-[14px]"> ${el}</span>`,
+      `<span class="bg-[#FDE68A] rounded-full px-3 text-[14px]"> ${el.toUpperCase()}</span>`,
   );
   return htmlElements.join(" ");
 };
@@ -113,4 +116,87 @@ function showLoading() {
 function hideLoading() {
   loadingSpinner.classList.add("hidden");
   allIssueContainer.classList.remove("hidden");
+}
+
+async function openIssueModal(issueId) {
+  const res = await fetch(
+    `https://phi-lab-server.vercel.app/api/v1/lab/issue/${issueId}`,
+  );
+
+  const data = await res.json();
+
+  const issueDetails = data.data;
+
+  issueDetailsModal.innerHTML = "";
+
+  const dateObj = new Date(issueDetails.createdAt);
+  const year = dateObj.getFullYear();
+  const month = String(dateObj.getMonth() + 1).padStart(2, "0");
+  const day = String(dateObj.getDate()).padStart(2, "0");
+  const formattedDate = `${month}/${day}/${year}`;
+
+  const issueDetailsCard = document.createElement("div");
+  issueDetailsCard.className = "modal-box";
+  issueDetailsCard.innerHTML = `
+     <div class="bg-white p-6 rounded-lg">
+            <!-- Title -->
+            <h2 class="text-2xl font-bold mb-2">
+              ${issueDetails.title}
+            </h2>
+
+            <div class="flex items-center gap-3 text-sm text-gray-500 mb-4">
+              <span
+                class="bg-green-500 text-white px-3 py-1 rounded-full text-xs"
+              >
+                ${issueDetails.status}
+              </span>
+
+              <span>•</span>
+
+              <span>Opened by ${issueDetails.author}</span>
+
+              <span>•</span>
+              <p>${formattedDate}</p>
+            </div>
+
+           
+            <div class="flex gap-3 mb-4">
+               ${createElements(issueDetails.labels)}
+            </div>
+
+         
+            <p class="text-gray-600 mb-6">
+              ${issueDetails.description}
+            </p>
+
+         
+            <div class="bg-base-200 flex justify-between items-center p-4">
+    
+              <div>
+                <p class="text-sm text-gray-500">Assignee:</p>
+                <p class="font-semibold"> ${issueDetails.assignee}</p>
+              </div>
+
+  
+              <div>
+                <p class="text-sm text-gray-500">Priority:</p>
+                <span
+                  class="bg-red-500 text-white px-4 py-1 rounded-full text-xs"
+                >
+                  ${issueDetails.priority.toUpperCase()}
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div class="modal-action">
+            <form method="dialog">
+              <!--if there is a button in form, it will close the modal-->
+              <button class="btn btn-primary border-none">Close</button>
+            </form>
+          </div>
+  `;
+  issueDetailsModal.appendChild(issueDetailsCard);
+
+  issueDetailsModal.showModal();
 }
